488102bb0615421e998fa812e79a96d6efe3264a
Enoch Kwak <enkwak@c4r3s12.42tokyo.jp>
2026-05-27 18:11:05 +0900

    solve

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -4,6 +4,8 @@
 #include <vector>
 #include <string>
 #include <cmath>
+#include <queue>
+#include <unordered_set>
 
 using namespace std;
 
@@ -20,10 +22,89 @@ class Timer {
 		}
 };
 
+int dy[] = {-1, 0, 1, 0};
+int dx[] = {0, 1, 0, -1};
+
 struct State {
 	int size;
 	vector<int> board;
 };
+
+struct Node {
+	vector<int> board;
+	uint32_t parents_index;
+	int h_cost;
+	int g_cost;
+	int zero_pos;
+
+	int f() const {
+		return h_cost + g_cost;
+	}
+};
+
+class MemoryPool {
+	vector<Node> pool;
+	public:
+		MemoryPool(size_t initial_capacity = 1000000) {
+			pool.reserve(initial_capacity);
+		}
+
+		uint32_t allocate(vector<int>& board, uint32_t parent, int h, int g, int zero) {
+			if (pool.size() == pool.capacity()) {
+				pool.reserve(pool.capacity() * 2);
+				uint32_t index = pool.size();
+				pool.push_back({board, parent, h, g, zero});
+				return index;
+			}
+			return (uint32_t)pool.size();
+		}
+
+		const Node& get(uint32_t index) const {
+			return pool[index];
+		}
+
+		Node& get(uint32_t index) {
+			return pool[index];
+		}
+
+		size_t size() const {
+			return pool.size();
+		}
+};
+
+struct BoardHash {
+	size_t operator()(const vector<int>& board) const {
+		size_t hash = 14695981039346656037ull; // FNV hash offset
+		for (int val : board) {
+			hash ^= val;
+			hash *= 1099511628211ull; // FNV hash prime
+		}
+		return hash;
+	}
+};
+
+struct BoardEqual {
+	bool operator()(const vector<int>& a, const vector<int>& b) const {
+		return a == b;
+	}
+};
+
+struct CompareNode {
+	MemoryPool* pool;
+	CompareNode(MemoryPool* p) : pool(p) {}
+
+	bool operator()(uint32_t a, uint32_t b) const {
+		const Node& na = pool->get(a);
+		const Node& nb = pool->get(b);
+		if (na.f() == nb.f()) {
+			return na.h_cost > nb.h_cost;
+		}
+		return na.f() > nb.f();
+	}
+};
+// 使い方:
+// CompareNode comp(&pool);
+// std::priority_queue<uint32_t, vector<uint32_t>, CompareNode> open_set(comp);
 
 bool loadPuzzle(const string& filename, State& out_state) {
 	ifstream file(filename);
@@ -201,6 +282,74 @@ bool checkSolvable(State& init, ManhattanDistance& md) {
 	}
 }
 
+void solvePuzzle(State init, ManhattanDistance md) {
+	MemoryPool mp;
+	CompareNode comp(&mp);
+	priority_queue<uint32_t, vector<uint32_t>, CompareNode> open_set(comp);
+	unordered_set<vector<int>, BoardHash, BoardEqual> closed_set;
+
+	int zero = -1;
+	for (int i = 0; i < (int)init.board.size(); ++i) {
+		if (init.board[i] == 0) {
+			zero = i;
+			break;
+		}
+	}
+	uint32_t start_idx = mp.allocate(init.board, UINT32_MAX, md.dist(init.board, init.size), 0, zero);
+	open_set.push(start_idx);
+
+	uint32_t goal_idx = UINT32_MAX;
+
+	while (!open_set.empty()) {
+		uint32_t current_idx = open_set.top();
+		open_set.pop();
+
+		const Node& nd = mp.get(current_idx);
+
+		if (nd.h_cost == 0) {
+			goal_idx = current_idx;
+			break;
+		}
+		if (closed_set.contains(nd.board)) continue;
+		closed_set.insert(nd.board);
+		int z_y = nd.zero_pos / init.size;
+		int z_x = nd.zero_pos % init.size;
+
+		for (int i = 0; i < 4; ++i) {
+			int ny = z_y + dy[i];
+            int nx = z_x + dx[i];
+
+			if (!check(ny, nx, init.size)) continue;
+
+			int new_zero = ny * init.size + nx;
+
+			vector<int> neighbor_board = nd.board;
+			swap(neighbor_board[nd.zero_pos], neighbor_board[new_zero]);
+			if (closed_set.contains(neighbor_board)) continue;
+
+			int new_g = nd.g_cost + 1;
+            int new_h = md.dist(neighbor_board, init.size);
+
+			uint32_t neighbor_idx = mp.allocate(neighbor_board, current_idx, new_h, new_g, new_zero);
+            open_set.push(neighbor_idx);	
+		}
+	}
+
+	if (goal_idx != UINT32_MAX) {
+		vector<uint32_t> path;
+		uint32_t curr = goal_idx;
+
+		while (curr != UINT32_MAX) {
+			path.push_back(curr);
+			curr = mp.get(curr).parents_index;
+		}
+
+		cout << "Solved in " << path.size() - 1 << " moves!" << endl;
+		cout << "Time complexity (Total states selected): " << closed_set.size() << endl;
+		cout << "Size complexity (Max states in memory): " << mp.size() << endl;
+	}
+}
+
 int main(int argc, char ** argv) {
 	if (argc != 2) {
 		cerr << "Usage: " << argv[0] << " <puzzle_file>" << endl;
@@ -217,6 +366,7 @@ int main(int argc, char ** argv) {
 		return 0;
 	}
 	// TODO: 1. 解の存在判定
+	solvePuzzle(init, md);
     // TODO: 2. A* 探索アルゴリズムの実行
     // TODO: 3. 結果の出力
 	cout << "Execution Time: " << timer.elapsed() << "ms" << endl;
